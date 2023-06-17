d21594e3d7b97cb1d4e2936c5fa5f13141f2e6e6
Jorge_dany <spumasc11@gmail.com>
2023-06-17 02:38:00 -0500

    VelcimetroSInFuncionar

--- Juego/DriveChain/Assets/Scripts/CarController.cs
+++ Juego/DriveChain/Assets/Scripts/CarController.cs
@@ -11,24 +11,35 @@ public class CarController : MonoBehaviour
 
     public float motorTorque = 500f; // Torque del motor
     public float maxSteeringAngle = 30f; // Ángulo máximo de giro
+    float steeringAngle;
+    float maxSpeed=0;
+    float torque=0;
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
+        steeringAngle = maxSteeringAngle * horizontalInput;  
         // Control del giro de las ruedas delanteras
-        float steeringAngle = maxSteeringAngle * horizontalInput;
+        
         frontLeftWheel.steerAngle = steeringAngle;
         frontRightWheel.steerAngle = steeringAngle;
 
         // Control de la tracción de las ruedas traseras
-        float torque = motorTorque * verticalInput;
-        rearLeftWheel.motorTorque = torque;
-        rearRightWheel.motorTorque = torque;
+        if(maxSpeed<=ppSO.speed){
+            torque = motorTorque * verticalInput; 
+            rearLeftWheel.motorTorque = torque;
+            rearRightWheel.motorTorque = torque;
+        }
+        else
+        {
+            rearLeftWheel.motorTorque = 0;
+            rearRightWheel.motorTorque = 0;
+        }
+        
         // Obtener la velocidad máxima
-        float maxSpeed = Mathf.Max(frontLeftWheel.rpm, frontRightWheel.rpm, rearLeftWheel.rpm, rearRightWheel.rpm) * 2 * Mathf.PI * rearLeftWheel.radius / 60;
-       // Debug.Log("Velocidad máxima: " + maxSpeed + " m/s");    
+        maxSpeed = Mathf.Max( rearLeftWheel.rpm, rearRightWheel.rpm) * 2 * Mathf.PI * rearLeftWheel.radius / 60;
+       Debug.Log("Velocidad máxima: " + maxSpeed + " m/s");    
     }
     private void OnCollisionEnter(Collision collision)
     {
